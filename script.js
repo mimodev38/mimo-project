4074370f5cb7b4275a49ab4ec2845f18be166f0e
mimodev38 <m.moneeka85@gmail.com>
2026-06-20 14:09:52 +0200

    script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ processBtn.addEventListener('click', async () => {
   });
 
   try {
-    const res = await fetch('/api/chat', {
+    const res = await fetch('/backend-chat', {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify({
